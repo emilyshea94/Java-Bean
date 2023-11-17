d04b0f9ae913af1c268a58dfc342df1a2fdc3ecd
emily shea <emilyshea947@gmail.com>
2023-11-17 00:12:41 -0500

    Added some var

--- assets/script java.js
+++ assets/script java.js
@@ -7,12 +7,20 @@ function writePassword() {
   var length = 
 
 }
-//var password? 
+//var password? trying out math  
+// dont forget to use . when using a variable
+var password = "";
+for (var i = 0; i < length; i++) {
+  var randomIndex + Math.floor (Math.random() *charset.length);
+  password += chars[randomIndex];
+}
 
+//function and query selector repeated again here. remember to use var
+//dont forget semicolons this time!!
 function writePassword() {
   var password= generatePassword();
   var passwordText = document.querySelector("passsword");
-  passwordText.value = password
+  passwordText.value = password;
 }
 
       // Add event listener to generate button
