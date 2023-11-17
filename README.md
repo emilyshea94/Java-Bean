5783290bf974e9b210539be2c688d698e71bac9c
emily shea <emilyshea947@gmail.com>
2023-11-16 20:25:54 -0500

    First Commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Java-Bean
 P.A.S.S.W.O.R.D
+
+For this task I modified code to make a working password generator that generates a password with 8-128 characacters and displays the password as a alert writen on the page. I chose to lowercase, uppercase, numeric, and/or a special character. 
+This application generates a randomized password when you click on the "generate password" button. 
+
+
+
+
--- assets/script java.js
+++ assets/script java.js
@@ -1,9 +1,13 @@
 // Assignment Code
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = "";
+  var passwordText= document.getElementsByName ("password")
+  var charLength= window.prompt (password )
+  generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
