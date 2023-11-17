61340f8a7243b68226dc5ef97cd195f9b92ce92e
emily shea <emilyshea947@gmail.com>
2023-11-16 23:20:56 -0500

    moved java

--- assets/script java.js
+++ assets/script java.js
@@ -13,6 +13,18 @@ function writePassword() {
   passwordText.value = password;
 
 }
+//8-128 characers, letters and symbols
+//try out math? 
+function generatePassword() {
+  var length = 8-,
+      charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+      retVal = "";
+  for (var i = 0, n = charset.length; i < length; ++i) {
+      retVal += charset.charAt(Math.floor(Math.random() * n));
+  }
+  return retVal;
+}
 
-// Add event listener to generate button
+
+      // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
